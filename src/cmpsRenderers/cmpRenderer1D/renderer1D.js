2b89c40b8d5c0a765a484ba0211975bdb1c169b1
Damecek <stepanek.ada@seznam.cz>
2020-05-10 00:35:10 +0200

    workin history 1d, a bit

--- src/cmpsRenderers/cmpRenderer1D/renderer1D.js
+++ src/cmpsRenderers/cmpRenderer1D/renderer1D.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react'
 import '../renderer.css';
 
 export class Renderer1D extends Component {
+    static history = [];
+
+    constructor(props) {
+        super(props);
+        // Renderer1D.history.push(this.props.obj);
+    }
+
+    componentDidUpdate() {
+        Renderer1D.history.push(this.props.obj);
+    }
+
     render() {
         function color(item) {
             return item === 0 ? "white" : "black";
         }
+        console.log(Renderer1D.history);
+        
         const length = this.props.obj.length; //obj is array from game(1d,2d,3d)
         const size = length > 34 ? (1140/length).toString() + 'px': '34px';
         return (
             <div>
-                {this.props.obj.map((item, i) =>{
-                    return <span className={`square ${color(item)}`} key={i} style={{width: size, height: size}}></span>;
+                {Renderer1D.history.map((line, i)=>{
+                    return (
+                        <div key={i}>
+                            {line.map((item, i)=>{
+                                return <span className={`square ${color(item)}`} key={i} style={{width: size, height: size}}></span>
+                            })}<br/>
+                        </div>
+                    )
                 })}
             </div>
         )
