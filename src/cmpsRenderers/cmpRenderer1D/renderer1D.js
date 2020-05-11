81f5a7741e80dc93bf6d19b4e0f8a0e34cc9cff3
Damecek <stepanek.ada@seznam.cz>
2020-05-11 17:56:29 +0200

    reomved border between lines

--- src/cmpsRenderers/cmpRenderer1D/renderer1D.js
+++ src/cmpsRenderers/cmpRenderer1D/renderer1D.js
@@ -17,12 +17,8 @@ export class Renderer1D extends Component {
         function color(item) {
             return item === 0 ? "white" : "black";
         }
-        console.log(this.state.history);
-        
         
         const length = this.props.obj.length; //obj is array from game
-        // const size = length > 36 ? (245/length).toString() + 'vh': '7vh';
-        // const size = (245/length).toString() + 'vh';
         const size = (100/length).toFixed(20) + '%';
         return (
             <div className="w-100 justify-content-center">
@@ -30,9 +26,8 @@ export class Renderer1D extends Component {
                     return (
                         <div key={i} className="reset-line-height">
                             {line.map((item, i)=>{
-                                // return <span className={`square ${color(item)}`} key={i} style={{width: size, height: size}}></span>
                                 return <span className={`square ${color(item)}`} key={i} style={{width: size, paddingTop: size}}></span>
-                            })}<br/>
+                            })}
                         </div>
                     )
                 })}
